5749f8182de9688f3e690d939ca73fcee054e130
Delta Mike <delta_michael@protonmail.com>
2024-09-19 15:36:24 +0300

    Experiment kind of successful

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ use nix::unistd::{close, read, write};
 use std::str::FromStr;
 
 const K_MAX_MSG: usize = 4096;
-
 // Guard against partial writes
 fn write_full(fd: RawFd, wbuf: &mut[u8]) -> Result<usize, Errno> {
     let mut buf_start = 0;
@@ -60,31 +59,31 @@ fn query(fd: RawFd, text: &str) -> Result<usize, Errno> {
     let length = u32::try_from(reply.len()).unwrap();
     wbuf[0..4].copy_from_slice(&length.to_le_bytes());
     wbuf[4..4 + reply.len()].copy_from_slice(reply);
-    write_full(fd, &mut wbuf[0..4 + reply.len()])?;
+    write_full(fd, &mut wbuf[0..4 + reply.len()])// ?;
 
-    let mut len_buf: [u8; 4] = [0; 4];
-    let length;
-    let mut rbuf: [u8; K_MAX_MSG] = [0; K_MAX_MSG];
-    match read_full(fd, &mut len_buf) {
-        Ok(_) => {
-            length = u32::from_le_bytes(len_buf);
-        },
-        Err(e) => {
-            println!("read() error {}", e);
-            return Err(e);
-        }
-    }
+    // let mut len_buf: [u8; 4] = [0; 4];
+    // let length;
+    // let mut rbuf: [u8; K_MAX_MSG] = [0; K_MAX_MSG];
+    // match read_full(fd, &mut len_buf) {
+    //     Ok(_) => {
+    //         length = u32::from_le_bytes(len_buf);
+    //     },
+    //     Err(e) => {
+    //         println!("read() error {}", e);
+    //         return Err(e);
+    //     }
+    // }
 
-    match read_full(fd, &mut rbuf[..length.try_into().unwrap()]) {
-        Ok(_) => {
-            println!("Server says {}", String::from_utf8(rbuf[..length.try_into().unwrap()].to_vec()).unwrap());
-        }
-        Err(e) => {
-            println!("read() error {}", e);
-            return Err(e);
-        }
-    }
-    Ok(0)
+    // match read_full(fd, &mut rbuf[..length.try_into().unwrap()]) {
+    //     Ok(_) => {
+    //         println!("Server says {}", String::from_utf8(rbuf[..length.try_into().unwrap()].to_vec()).unwrap());
+    //     }
+    //     Err(e) => {
+    //         println!("read() error {}", e);
+    //         return Err(e);
+    //     }
+    // }
+    // Ok(0)
 }
 
 fn main() {
@@ -95,9 +94,9 @@ fn main() {
             let localhost = SockaddrIn::from_str("127.0.0.1:8081").unwrap();
             match connect(fd, &localhost) {
                 Ok(()) => {
-                    let _ = query(fd, "hello1");
-                    let _ = query(fd, "hello2");
-                    let _ = query(fd, "hello3");
+                    let _ = query(fd, "helloagainfriendofafriendiknewyouwhen1");
+                    let _ = query(fd, "helloagainfriendofafriendiknewyouwhen2");
+                    let _ = query(fd, "helloagainfriendofafriendiknewyouwhen3");
                 },
                 Err(e) => {
                     println!("Error connecting to server {}", e);
