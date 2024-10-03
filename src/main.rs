9dffa8c8b7c0655c57a30d690492ef3586f9cc6d
Delta Mike <delta_michael@protonmail.com>
2024-10-03 18:09:58 +0300

    Ch-7: Basic server

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use nix::errno::Errno;
 use std::os::fd::RawFd;
 use nix::unistd::{close, read, write};
 use std::str::FromStr;
+use std::env;
 
 const K_MAX_MSG: usize = 4096;
 
@@ -65,7 +66,9 @@ fn send_request(fd: RawFd, text: &str) -> Result<usize, Errno> {
 
 fn read_response(fd: RawFd) -> Result<usize, Errno> {
     let mut len_buf: [u8; 4] = [0; 4];
-    let length;
+    let mut res_code_buf: [u8; 4] = [0; 4];
+    let mut length;
+    let res_code: u32;
     let mut rbuf: [u8; K_MAX_MSG] = [0; K_MAX_MSG];
     match read_full(fd, &mut len_buf) {
         Ok(_) => {
@@ -77,14 +80,29 @@ fn read_response(fd: RawFd) -> Result<usize, Errno> {
         }
     }
 
-    match read_full(fd, &mut rbuf[..length.try_into().unwrap()]) {
+    match read_full(fd, &mut res_code_buf) {
         Ok(_) => {
-            println!("Server says {}", String::from_utf8(rbuf[..length.try_into().unwrap()].to_vec()).unwrap());
-        }
+            res_code = u32::from_le_bytes(res_code_buf);
+        },
         Err(e) => {
             println!("read() error {}", e);
             return Err(e);
         }
+    }
+
+    length -= 4;
+    if length > 0 {
+        match read_full(fd, &mut rbuf[..length.try_into().unwrap()]) {
+            Ok(_) => {
+                println!("[{}] {}", res_code, String::from_utf8(rbuf[..length.try_into().unwrap()].to_vec()).unwrap());
+            }
+            Err(e) => {
+                println!("read() error {}", e);
+                return Err(e);
+            }
+        }
+    } else {
+        println!("[{}]", res_code);
     }
     Ok(0)
 }
@@ -96,24 +114,21 @@ fn query(fd: RawFd, text: &str) -> Result<usize, Errno> {
 
 fn main() {
     let fd = socket(AddressFamily::Inet, SockType::Stream, SockFlag::empty(), None);
-
+    let mut args: Vec<String> = env::args().collect();
+    args.remove(0);
+    let req = args.join(" ");
     match fd {
         Ok(fd) => {
             let localhost = SockaddrIn::from_str("127.0.0.1:8081").unwrap();
             match connect(fd, &localhost) {
                 Ok(()) => {
-                    let requests = ["hello1", "hello2", "hello3"];
-                    for r in requests.iter() {
-                        if let Err(e) = send_request(fd, r) {
-                            println!("Error {} sending request {}", e, r);
-                            let _ = close(fd);
-                            return;
-                        }
+                    if let Err(e) = send_request(fd, &req) {
+                        println!("Error {} sending request {}", e, req);
+                        let _ = close(fd);
+                        return;
                     }
-                    for _ in 0..3 {
-                        if let Err(_) = read_response(fd) {
-                            return;
-                        }
+                    if let Err(_) = read_response(fd) {
+                        return;
                     }
                 },
                 Err(e) => {
